91d699318ee86c210b1951b49ffc50a13558a565
mukhit <mukhit.n@1fit.app>
2024-04-13 14:59:36 +0600

    fix: return token

--- controllers/authController.js
+++ controllers/authController.js
@@ -52,7 +52,7 @@ const login = async (req, res) => {
     const token = createToken(user._id);
 
     res.cookie("token", token, { maxAge: maxAge * 1000 });
-    res.status(200).json({ user: user._id });
+    res.status(200).json({ user: user._id, token });
   } catch (err) {
     res.status(400).json({ message: err.message });
   }
